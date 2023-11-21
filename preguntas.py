757ade86985262cf3f9b0d621b6bf241eef22823
“lauracamilaballesteros” <lcballesteroso@unal.edu.co>
2023-11-20 21:48:26 -0500

    fin

--- preguntas.py
+++ preguntas.py
@@ -34,10 +34,10 @@ def pregunta_01():
     X_reshaped = X.reshape(139,1)
 
     # Imprima las nuevas dimensiones de `y`
-    print(y_reshaped)
+    print(y_reshaped.shape)
 
     # Imprima las nuevas dimensiones de `X`
-    print(y_reshaped)
+    print(y_reshaped.shape)
 
 
 def pregunta_02():
